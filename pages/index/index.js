33f9c61e389e08222955d3e8d5f5d96f631853e8
ALBAZ666 <ALBAZ666@users.noreply.github.com>
2026-07-03 20:08:08 +0800

    条件渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,9 @@ Page({
     age: 19,
     userinfo: { name: 'lqz', hobby: '烫头' },
     names: ['刘亦菲', '迪丽热巴', '古力娜扎', '马儿咋哈'],
-    goodsList:[{id:1001,name:'钢笔',price:9},{id:1002,name:'铅笔',price:6},{id:1003,name:'脸盆',price:99}]
+    goodsList: [{ id: 1001, name: '钢笔', price: 9 }, { id: 1002, name: '铅笔', price: 6 }, { id: 1003, name: '脸盆', price: 99 }],
+    showPhoto: false,
+    score: 0
   },
 
   handleAddAge() {
@@ -25,6 +27,12 @@ Page({
     this.data.names.push('亚瑟王')
     this.setData({
       names: this.data.names
+    })
+  },
+
+  handleShowPhoto() {
+    this.setData({
+      showPhoto: !this.data.showPhoto
     })
   }
 })
