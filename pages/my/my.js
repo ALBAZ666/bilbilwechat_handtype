3b471c65a3c325bba138a788ea405c6b4b49d3de
ALBAZ666 <ALBAZ666@users.noreply.github.com>
2026-07-03 12:33:57 +0800

    编程式页面跳转

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,17 +1,32 @@
-page({
-handleConsole(){
-   console.log('我被点了')
-},
-handleParent(){
-  console.log("父亲被点了")
-},
-handleChildren(){
-  console.log("孩子被点了")
-},
-handleClick01(event){
-  console.log(event)
-  console.log(event.target.dataset)
-  console.log(event.target.dataset.name)
-  console.log(event)
-}
+Page({
+  handleConsole() {
+    console.log('我被点了')
+  },
+  handleParent() {
+    console.log("父亲被点了")
+  },
+  handleChildren() {
+    console.log("孩子被点了")
+  },
+  handleChick01(event) {
+    console.log(event)
+    console.log(event.target.dataset)
+    console.log(event.target.dataset.name)
+    console.log(event)
+  },
+  handlenavigateTo() {
+    wx.navigateTo({ url: '/pages/login/login' })
+  },
+  handleredirectTo() {
+    wx.redirectTo({ url: '/pages/login/login' })
+  },
+  handleswichTab() {
+    wx.switchTab({ url: '/pages/index/index' })
+  },
+  handlereLaunch() {
+    wx.reLaunch({ url: '/pages/index/index' })
+  },
+  handlenavigateBack() {
+    wx.navigateBack({ delta: 1 })
+  }
 })
