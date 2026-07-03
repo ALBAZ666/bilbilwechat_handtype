57785bf671abdf31518eb4cc50fec58c3841c55c
ALBAZ666 <ALBAZ666@users.noreply.github.com>
2026-07-03 19:14:17 +0800

    模板语法之数据更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,29 @@
 // index.js
 Page({
-  handleConsole(){
-     console.log('我被点了')
+  data: {
+    name: 'justin',
+    age: 19,
+    userinfo: { name: 'lqz', hobby: '烫头' },
+    names: ['刘亦菲', '迪丽热巴', '古力娜扎', '马儿咋哈']
   },
-  
-  })
-  var name="lqz"
-  var obj={
-  name,
-  handleConsole(){},
+
+  handleAddAge() {
+    let a = this.data.age + 1
+    this.setData({
+      age: a
+    })
+  },
+
+  handleChangeHobby() {
+    this.setData({
+      'userinfo.hobby': '篮球'
+    })
+  },
+
+  handleAddName() {
+    this.data.names.push('亚瑟王')
+    this.setData({
+      names: this.data.names
+    })
   }
+})
